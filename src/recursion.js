83242842705c88372b8083d92fe148bf3affe1e3
pknipp <61242672+pknipp@users.noreply.github.com>
2020-03-05 10:46:29 -0500

    Fix palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -123,6 +123,16 @@ var reverse = function(string) {
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
+	string = string.split(" ").join("").toLowerCase();
+	if(string.length === 0 || string.length === 1){
+		return true;
+	}else{
+		if(string[0] === string[string.length - 1]){
+			return palindrome(string.slice(1,-1));
+		}else{
+			return false;
+		}
+	}
 };
 
 // 11. Write a function that returns the remainder of x divided by y without using the
