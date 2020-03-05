59caff6dc0ae5af649c27ba3614bcea9c381741f
pknipp <61242672+pknipp@users.noreply.github.com>
2020-03-05 12:02:48 -0500

    Fix multiply

--- src/recursion.js
+++ src/recursion.js
@@ -157,6 +157,17 @@ var modulo = function(x, y) {
 // 12. Write a function that multiplies two numbers without using the * operator or
 // Math methods.
 var multiply = function(x, y) {
+	if(x === 0 || y === 0){
+		return 0;
+	}else if(x < 0){
+		return -multiply(-x,y);
+	}else if(y < 0){
+		return -multiply(x,-y);
+	} if(y === 1){
+		return x;
+	}else{
+		return x + multiply(x, y-1);
+	}
 };
 
 // 13. Write a function that divides two numbers without using the / operator or
